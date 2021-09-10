714014fd89c7fb0b5af318eb2b86f787ba2bfacc
edisson <osorioedisson08@gmail.com>
2021-09-10 12:25:04 -0500

    [ADD]changes

---
+++ pdf/src/componets/Post.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+
+class Post extends Component {
+    state = {
+        title: "",
+        content: "",
+        Image: "",
+        postSubmitted: false
+    }
+    render() {
+        return (
+            <>
+                hi!
+            </>
+        );
+    }
+}
+export default Post;
